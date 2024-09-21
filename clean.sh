51bed81b8fc2e2778d4b296cb780b54b4d8efe78
Mohamed Serag El-din <69184709+medoston@users.noreply.github.com>
2024-09-21 15:40:17 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #clean docker client (delete all images, containers, volumes)
-sudo su - user <<eos               
+sudo su - root <<eos               
 docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi --force $(docker images -q) && docker volume ls -q | xargs -r docker volume rm -f              
 docker system prune -a -y               
 apt-get update             
